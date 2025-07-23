093165479f2686df227d89d4a6131fae820d7097
PrachiiAgarwal <pagarwal2611@gmail.com>
2025-07-23 03:10:28 +0000

    HTML basics

---
+++ script.js
@@ -0,0 +1,4 @@
+// In HTML h1, h2, h3 is used to maintain the hierarchy in headings and sub headings
+// In HTML lorem23 or lorem10 whatever is used to create  placeholder for the developers to design the website matlab ek khali space create kr deta h jisko hum clients ko de skte to modify there website
+// In HTML if we use style attribute that is style= bcakground red then this is name as inline css
+//anchor tag in HTML is used to create links
